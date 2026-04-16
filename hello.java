5cd0a0aa77c41124b35610c5b6abd1f5bd9dc5fd
duraivenkat <saravananduraivenkat@gmail.com>
2026-04-16 16:09:30 +0530

    durai2

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello
 {
     public static void main(String args[])
     {
-        // madeing some changes
+        // madeing some changes.
         System.out.println("hello my world");
         
     }
--- temp.java
+++ temp.java
@@ -1,5 +1,6 @@
 public class temp {
     public static void main(String[] args) {
+        //haii
         System.out.println("haii ");
     }
 }
