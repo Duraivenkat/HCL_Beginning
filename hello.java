7165bc334b03052b1d85f56fda38f6132b9b3de9
duraivenkat <saravananduraivenkat@gmail.com>
2026-04-17 14:14:24 +0530

    two

--- hello.java
+++ hello.java
@@ -3,8 +3,9 @@ public class hello
     public static void main(String args[])
     {
     int a=10;
+    int b=10;
 
-        System.out.println(a);
+        System.out.println(a+b);
         
     }
 }
